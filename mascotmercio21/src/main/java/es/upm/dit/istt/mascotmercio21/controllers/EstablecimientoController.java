851ef5fcfdb3219aa6aa53d5f3f2929e65e5b811
Victorino06 <105801281+Victorino06@users.noreply.github.com>
2024-04-04 10:46:04 +0200

    Update EstablecimientoController.java

--- mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/controllers/EstablecimientoController.java
+++ mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/controllers/EstablecimientoController.java
@@ -1,1 +1,53 @@
+package es.upm.dit.istt.mascotmercio21.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/establecimientos")
+public class EstablecimientoController {
+
+    @Autowired
+    private EstablecimientoRepository establecimientoRepository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Establecimiento> obtenerEstablecimiento(@PathVariable Integer id) {
+        Establecimiento establecimiento = establecimientoRepository.findById(id).orElse(null);
+        if (establecimiento != null) {
+            return ResponseEntity.ok(establecimiento);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Establecimiento> agregarEstablecimiento(@RequestBody Establecimiento establecimiento) {
+        Establecimiento nuevoEstablecimiento = establecimientoRepository.save(establecimiento);
+        return ResponseEntity.ok(nuevoEstablecimiento);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Establecimiento> actualizarEstablecimiento(@PathVariable Integer id, @RequestBody Establecimiento establecimiento) {
+        Establecimiento establecimientoExistente = establecimientoRepository.findById(id).orElse(null);
+        if (establecimientoExistente != null) {
+            establecimiento.setId(id);
+            Establecimiento establecimientoActualizado = establecimientoRepository.save(establecimiento);
+            return ResponseEntity.ok(establecimientoActualizado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarEstablecimiento(@PathVariable Integer id) {
+        Establecimiento establecimientoExistente = establecimientoRepository.findById(id).orElse(null);
+        if (establecimientoExistente != null) {
+            establecimientoRepository.delete(establecimientoExistente);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
 
