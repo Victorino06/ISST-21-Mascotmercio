a99bedda3d9ee2e1575388bae12063259b85ac7f
Victorino06 <105801281+Victorino06@users.noreply.github.com>
2024-04-04 11:39:16 +0200

    Update ClienteRepository.java

--- mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/repository/ClienteRepository.java
+++ mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/repository/ClienteRepository.java
@@ -1,1 +1,13 @@
+package es.upm.dit.istt.mascotmercio21.respository
+
+import es.upm.dit.istt.mascotmercio21.models.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    List<Cliente> findAll();
+    Cliente findById(Integer id);
+}
 
