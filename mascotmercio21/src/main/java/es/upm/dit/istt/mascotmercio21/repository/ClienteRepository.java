c7a9df77783d71f2193e24504e714e65950fdfc3
Victorino06 <105801281+Victorino06@users.noreply.github.com>
2024-04-04 13:23:49 +0200

    Update ClienteRepository.java

--- mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/repository/ClienteRepository.java
+++ mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/repository/ClienteRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
-
 @Repository
 public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
     List<Cliente> findAll();
