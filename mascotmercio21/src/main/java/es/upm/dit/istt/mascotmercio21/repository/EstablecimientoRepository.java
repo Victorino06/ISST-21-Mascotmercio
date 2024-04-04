cfa4a3a6313f0cfa44287175e0438624e3a9c070
Victorino06 <105801281+Victorino06@users.noreply.github.com>
2024-04-04 11:34:29 +0200

    Update EstablecimientoRepository.java

--- mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/repository/EstablecimientoRepository.java
+++ mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/repository/EstablecimientoRepository.java
@@ -1,1 +1,11 @@
+package es.upm.dit.istt.mascotmercio21.respository
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstablecimientoRepository extends JpaRepository<Establecimiento, Integer> {
+    // Puedes agregar métodos personalizados de consulta si es necesario
+}
 
