eb43162a9eccf05f0be12fb35e415181c0a48b62
Victorino06 <105801281+Victorino06@users.noreply.github.com>
2024-04-04 10:50:32 +0200

    Update EstablecimientoController.java

--- mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/controllers/EstablecimientoController.java
+++ mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/controllers/EstablecimientoController.java
@@ -1,5 +1,7 @@
 package es.upm.dit.istt.mascotmercio21.controllers;
 
+import es.upm.dit.istt.mascotmercio21.models.Establecimiento;
+import es.upm.dit.istt.mascotmercio21.repository.EstablecimientoRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
