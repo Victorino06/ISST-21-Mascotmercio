a387cb519c8c9cf8a2f89b8511ae9cbe5b2ed05e
Victorino06 <105801281+Victorino06@users.noreply.github.com>
2024-04-04 12:15:18 +0200

    Update PropietarioRepository.java

--- mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/repository/PropietarioRepository.java
+++ mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/repository/PropietarioRepository.java
@@ -1,12 +1,12 @@
 package es.upm.dit.istt.mascotmercio21.repository;
 
 import es.upm.dit.istt.mascotmercio21.models.Propietario;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface PropietarioRepository extends JpaRepository<Propietario, Integer> {
+public interface PropietarioRepository extends CrudRepository<Propietario, Integer> {
     List<Propietario> findAll();
-    Propietario findById(Integer id);
+   Optional <Integer> findById(Integer id);
 }
