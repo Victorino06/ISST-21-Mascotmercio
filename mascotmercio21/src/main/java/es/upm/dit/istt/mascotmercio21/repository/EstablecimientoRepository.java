c694c3582a6d3349bae657e37d3ec6803a366a88
Victorino06 <105801281+Victorino06@users.noreply.github.com>
2024-04-04 12:15:38 +0200

    Update EstablecimientoRepository.java

--- mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/repository/EstablecimientoRepository.java
+++ mascotmercio21/src/main/java/es/upm/dit/istt/mascotmercio21/repository/EstablecimientoRepository.java
@@ -1,11 +1,10 @@
 package es.upm.dit.istt.mascotmercio21.repository;
 
 import es.upm.dit.istt.mascotmercio21.models.Establecimiento;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EstablecimientoRepository extends JpaRepository<Establecimiento, Integer> {
+public interface EstablecimientoRepository extends CrudRepository<Establecimiento, Integer> {
     // Puedes agregar métodos personalizados de consulta si es necesario
 }
-
