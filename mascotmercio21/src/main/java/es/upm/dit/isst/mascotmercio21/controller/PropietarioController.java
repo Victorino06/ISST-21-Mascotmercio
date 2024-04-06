ccef755a3ca80d4228d91cfa454a1e0213469f27
Victorino06 <105801281+Victorino06@users.noreply.github.com>
2024-04-06 12:39:22 +0200

    Update PropietarioController.java

--- mascotmercio21/src/main/java/es/upm/dit/isst/mascotmercio21/controller/PropietarioController.java
+++ mascotmercio21/src/main/java/es/upm/dit/isst/mascotmercio21/controller/PropietarioController.java
@@ -4,6 +4,7 @@ import es.upm.dit.isst.mascotmercio21.model.Propietario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/propietarios")
